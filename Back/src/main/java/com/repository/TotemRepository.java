eb78a7dc2f53b8dddc7242efbe2146f03269c7bc
Jose <jose.poblete2001@alumnos.ubiobio.cl>
2025-01-09 12:48:38 -0300

    modelos

--- Back/src/main/java/com/repository/TotemRepository.java
+++ Back/src/main/java/com/repository/TotemRepository.java
@@ -1,15 +1,13 @@
 package com.repository;
 
+import com.models.Totem;
+
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.models.ERole;
-import com.models.Role;
-
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-  Optional<Role> findByName(ERole name);
+public interface TotemRepository extends JpaRepository<Totem, Long> {
+    Optional<Totem> findById(Long id);
 }
-
