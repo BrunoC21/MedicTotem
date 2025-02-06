18cc10c2179a50cb89d8d2dade048d01b0449f3c
Antonio Moncada <axteerix14@gmail.com>
2025-02-06 13:11:50 -0300

    Final

--- Back/src/main/java/com/controllers/MonitoresController.java
+++ Back/src/main/java/com/controllers/MonitoresController.java
@@ -34,32 +34,28 @@ public class MonitoresController {
     @GetMapping("/Sector1")
     public List<Ticket> listaTodosTicketsSector1() {
         return ticketRepository.findByTotemSector("Sector 1").stream()
-                .filter(ticket -> "Llamado".equals(ticket.getEstado()))
-                .filter(ticket -> "Preparacion".equals(ticket.getEstado()))
+                .filter(ticket -> "Llamado".equals(ticket.getEstado()) || "Preparacion".equals(ticket.getEstado()))
                 .collect(Collectors.toList());
     }
 
     @GetMapping("/Sector2")
     public List<Ticket> listaTodosTickets2() {
         return ticketRepository.findByTotemSector("Sector 2").stream()
-        .filter(ticket -> "Llamado".equals(ticket.getEstado()))
-        .filter(ticket -> "Preparacion".equals(ticket.getEstado()))
+        .filter(ticket -> "Llamado".equals(ticket.getEstado()) || "Preparacion".equals(ticket.getEstado()))
         .collect(Collectors.toList());
     }
 
     @GetMapping("/Sector4")
     public List<Ticket> listaTodosTickets4() {
         return ticketRepository.findByTotemSector("Sector 4").stream()
-        .filter(ticket -> "Llamado".equals(ticket.getEstado()))
-        .filter(ticket -> "Preparacion".equals(ticket.getEstado()))
+        .filter(ticket -> "Llamado".equals(ticket.getEstado()) || "Preparacion".equals(ticket.getEstado()))
         .collect(Collectors.toList());
     }
 
     @GetMapping("/Sector5")
     public List<Ticket> listaTodosTickets5() {
         return ticketRepository.findByTotemSector("Sector 5").stream()
-        .filter(ticket -> "Llamado".equals(ticket.getEstado()))
-        .filter(ticket -> "Preparacion".equals(ticket.getEstado()))
+        .filter(ticket -> "Llamado".equals(ticket.getEstado()) || "Preparacion".equals(ticket.getEstado()))
         .collect(Collectors.toList());
     }
 
